8df9a3be59145733fa2568acc4beb9f044f9b765
DannyFiller <tuanvatuan12321@gmail.com>
2025-03-23 19:57:39 +0700

    fix-responsive-navbar-va-skill-list

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,36 @@ function App() {
   };
   return (
     <>
-      <Navbar></Navbar>
+      <div
+        className="container d-flex justify-content-center p-2 my-4"
+        id="navbar-container"
+      >
+        {/* <div className='name'>Anh Khoi</div> */}
+        <div className="name-nav">Anh Khoi</div>
+        <ul className="nav">
+          <li className="nav-item p-2 mx-4">
+            <a onClick={() => handleScroll(profileRef)}>Profile</a>
+          </li>
+          <li className="nav-item p-2 mx-4">
+            <a onClick={() => handleScroll(skillRef)}>Skill</a>
+          </li>
+          <li className="nav-item p-2 mx-4">
+            <a onClick={() => handleScroll(projectRef)}>Project</a>
+          </li>
+          <li className="nav-item p-2 mx-4">
+            <a onClick={() => handleScroll(contactRef)}>Contact</a>
+          </li>
+          <li className="nav-item p-2 mx-4">
+            <div className="github-mobile">
+              <a href="https://github.com/DannyFiller">My GitHub</a>
+            </div>
+          </li>
+        </ul>
+        <div className="github">
+          <a href="https://github.com/DannyFiller">My GitHub</a>
+        </div>
+      </div>
+      ;{/* <Navbar></Navbar> */}
       <section ref={profileRef}>
         <Profile />
       </section>
@@ -43,28 +72,3 @@ function App() {
 }
 
 export default App;
-
-// <div
-//   className="container d-flex justify-content-center p-2 my-4"
-//   id="navbar-container"
-// >
-//   {/* <div className='name'>Anh Khoi</div> */}
-//   <div className="name-nav">Anh Khoi</div>
-//   <ul className="nav">
-//     <li className="nav-item p-2 mx-4">
-//       <a onClick={() => handleScroll(profileRef)}>Profile</a>
-//     </li>
-//     <li className="nav-item p-2 mx-4">
-//       <a onClick={() => handleScroll(skillRef)}>Skill</a>
-//     </li>
-//     <li className="nav-item p-2 mx-4">
-//       <a onClick={() => handleScroll(projectRef)}>Project</a>
-//     </li>
-//     <li className="nav-item p-2 mx-4">
-//       <a onClick={() => handleScroll(contactRef)}>Contact</a>
-//     </li>
-//   </ul>
-//   <div className="github">
-//     <a href="https://github.com/DannyFiller">My GitHub</a>
-//   </div>
-// </div>;
