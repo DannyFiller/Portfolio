47c030a43edbeb51ffa4b8f5e3e153f12cd41c50
DannyFiller <tuanvatuan12321@gmail.com>
2025-04-02 15:57:11 +0700

    add-lottie

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,17 +1,28 @@
+import Lottie from "react-lottie";
 import "./Profile.css";
+import animation from "../../assets/lotties/animation.json";
+
 const Profile = () => {
   return (
     <div id="container-profile">
-      <div className="container d-flex align-items-center justify-content-center m-4 p-4 w-50">
-        <img
+      {/* <div id="lottieBackground"></div> */}
+      <div className="img-container">
+        {/* <img
           className="img-wrapper"
           src="https://th.bing.com/th/id/R.c40ec7f46c93ed9dbcba2c6f982986f9?rik=JpmbD23mxP332w&riu=http%3a%2f%2fwww.lovethispic.com%2fuploaded_images%2f90113-Beautiful-Waterfall.jpg&ehk=Q0YlkUWuf3%2fK%2bpZwiZmQ324Hyhe%2bb4lhdYaShARqeWA%3d&risl=&pid=ImgRaw&r=0"
-        ></img>
+        ></img> */}
+        <div id="lottie1">
+          <Lottie
+            options={{ animationData: animation }}
+            speed={0.5}
+            isClickToPauseDisabled
+          />
+        </div>
       </div>
 
-      <div className="container d-flex flex-column align-items-center justify-content-center m-4 p-4 w-50">
-        <div className="name">Anh Khoi</div>
-        <div className="intro-1">Software Engineer</div>
+      <div className="name-container">
+        <div className="name">Xin chào, tui là Anh Khôi</div>
+        {/* <div className="intro-1">Software Engineer</div> */}
       </div>
     </div>
   );
